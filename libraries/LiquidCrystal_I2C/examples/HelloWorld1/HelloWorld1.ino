ef0962e67440e3a75353b69ad3b7bc04f06a90ba
王云凯 <615349118@163.com>
2017-07-29 16:29:20 +0800

    dht、LCD、DS3231
    dht是dht22专用库，LCD库为I2C接口的LCD专用库，DS3231是高精度时钟

---
+++ libraries/LiquidCrystal_I2C/examples/HelloWorld1/HelloWorld1.ino
@@ -0,0 +1,23 @@
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3f,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+ 
+  // Print a message to the LCD.
+  lcd.backlight();
+  delay(300);
+  lcd.print("Hello, world!");
+  lcd.noBacklight();
+  delay(300);
+}
+
+void loop()
+{
+  delay(1000);
+  setup();
+}
